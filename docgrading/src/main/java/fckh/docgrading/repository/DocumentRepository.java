c93ba9a04bbd00d68d32c746ddad84a89494434a
DaeSeob01 <161868263+DaeSeob01@users.noreply.github.com>
2025-06-09 16:56:36 +0900

    Delete docgrading directory

--- docgrading/src/main/java/fckh/docgrading/repository/DocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package fckh.docgrading.repository;
-
-import fckh.docgrading.domain.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-}
